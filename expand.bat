0842af4f215068595456a51407f738ceea07ca06
Richard Moore <51307516+Zatarita@users.noreply.github.com>
2022-08-25 18:43:31 -0400

    Create expand.bat

---
+++ expand.bat
@@ -0,0 +1,28 @@
+@echo off
+
+echo Extracting DDT Files...
+:: Run this twice as some dats hold other dtts
+for /r %%f in (*.dtt) do python dat.py "%%f"
+for /r %%f in (*.dtt) do python dat.py "%%f"
+
+echo Extracting DAT Files
+:: Run this twice as some dats hold other dats
+for /r %%f in (*.dat) do python dat.py "%%f"
+for /r %%f in (*.dat) do python dat.py "%%f"
+
+echo Extracting EFF Files...
+for /r %%f in (*.eff) do python dat.py "%%f"
+
+echo Extracting EVN Files...
+:: Run this twice as some evns have other evns
+for /r %%f in (*.evn) do python dat.py "%%f"
+for /r %%f in (*.evn) do python dat.py "%%f"
+
+echo Decompressing Z Files...
+for /r %%f in (*.z) do python z.py "%%f" "%%~dpnf.txt"
+
+echo Extracting TMD Files...
+for /r %%f in (*.tmd) do python tmd.py "%%f"
+
+echo Renaming WTP Files...
+for /r %%f in (*.wtp) do ren "%%f" *.dds
